66a6bd6186fb61485a48d18e8b465daa567e0b3e
Mitchell Blom <mitchellblom@gmail.com>
2017-05-14 15:39:30 -0500

    view toggle and form for add new

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,33 +4,42 @@ app.run((FIREBASE_CONFIG) => {
 
 app.controller("AddressControl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 	$scope.addresses = "Loading addresses...";
+	$scope.showListView = true;
 
-let getAddressesFromFb = () => {
-	let addresses = [];
-	return $q((resolve, reject) => {
-		$http.get(`${FIREBASE_CONFIG.databaseURL}/addresses.json`)
-			.then((fbItems) => {
-				var addressCollection = fbItems.data;
-				Object.keys(addressCollection).forEach((key) => {
-					addressCollection[key].id = key;
-					addresses.push(addressCollection[key]);
+	$scope.newContact = () => {
+		$scope.showListView = false;
+	};
+
+	$scope.allContacts = () => {
+		$scope.showListView = true;
+	};
+
+	let getAddressesFromFb = () => {
+		let addresses = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/addresses.json`)
+				.then((fbItems) => {
+					var addressCollection = fbItems.data;
+					Object.keys(addressCollection).forEach((key) => {
+						addressCollection[key].id = key;
+						addresses.push(addressCollection[key]);
+					});
+					resolve(addresses);
+				})
+				.catch((error) => {
+					reject(error);
 				});
-				resolve(addresses);
-			})
-			.catch((error) => {
-				reject(error);
-			});
-	});
-};
+		});
+	};
 
-let getAddressesToDom = () => {
-	getAddressesFromFb().then((addresses) => {
-		$scope.addresses = addresses;
-	}).catch((error) => {
-		console.log("get error", error);
-	});
-};
+	let getAddressesToDom = () => {
+		getAddressesFromFb().then((addresses) => {
+			$scope.addresses = addresses;
+		}).catch((error) => {
+			console.log("get error", error);
+		});
+	};
 
-getAddressesToDom();
+	getAddressesToDom();
 
 });
