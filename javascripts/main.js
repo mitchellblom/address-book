349929075af4288e71ab9575a37aa9ef697f156e
Mitchell Blom <mitchellblom@gmail.com>
2017-05-14 13:03:03 -0500

    app constants

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,9 @@
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(FIREBASE_CONFIG); 
+});
+
 var app = angular.module("AddressBook", []);	// make a new prototype from angular.min.js profile. array is for plugins
-											// name in quotes must be the same as the html. THE SAME!!!
 
-app.controller("ToDom", ($scope) => {			//quotes are name of controller, usually has ctrl in it in PaschalCase
+app.controller("AddressControl", ($scope) => {
 	$scope.something = "Something";						// angular variables to use go in parens
-});	
-
-
-
-// DO THIS::
-// Put variable in the controller and write it to the dom. Put this one in Exercises!
+});
