088bddca683e1cf7551b631f3b3f26db8f6def3c
Mitchell Blom <mitchellblom@gmail.com>
2017-05-23 12:13:51 -0500

    register and login

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,6 +1,29 @@
-app.run(function(FIREBASE_CONFIG) {
-	firebase.initializeApp(FIREBASE_CONFIG); 
-});																							// config runs once, run runs when any controller changes
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+  if(AuthFactory.isAuthenticated()){
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+app.run(function($location, $rootScope, FIREBASE_CONFIG, AuthFactory) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute) {
+    var logged = AuthFactory.isAuthenticated();
+
+    var appTo;
+
+    if (currRoute.originalPath) {
+      appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+    }
+
+    if (!appTo && !logged) {
+      event.preventDefault();
+      $location.path('/auth');
+    }
+  });
+});
 
 app.config(function($routeProvider){
     $routeProvider
@@ -29,5 +52,5 @@ app.config(function($routeProvider){
             controller: 'AuthCtrl',
             resolve: {isAuth}
         })
-        .otherwise('auth');
+        .otherwise('/auth');
 });
